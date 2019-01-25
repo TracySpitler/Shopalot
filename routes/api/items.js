ec870b001ba3ff5e51ccec26422e845a2ac020ea
Cordell Radke <csradke@fullsail.edu>
2019-01-24 20:15:09 -0800

    Created the simple back-end crud requirements for video two from the playlist.

---
+++ routes/api/items.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+// @desc Get All Items
+// @access Public
+router.get('/', (req, res) => {
+
+    Item.find()
+        .sort({ date: -1 })
+        .then(items => res.json(items));
+
+});
+
+// @route POST api/items
+// @desc Create an Item
+// @access Public
+router.post('/', (req, res) => {
+
+    const newItem = new Item({
+
+        name: req.body.name
+
+    });
+
+    newItem.save().then(item => res.json(item));
+
+});
+
+// @route DELETE api/items
+// @desc Delete an Item
+// @access Public
+router.delete('/:id', (req, res) => {
+
+    Item.findById(req.params.id)
+    //Callback returns a successful or unsuccessful item deletion
+        .then(item => item.remove().then(() => res.json({success: "Item deleted successfully"})))
+        .catch(err => res.status(404).json({error: "Error when deleting item"}));
+})
+
+
+
+module.exports = router;
