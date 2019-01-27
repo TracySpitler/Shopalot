49ff8cbd6a02aabdb3fa93bb96abf5afff154659
Tracy Spitler <tracyspitler@gmail.com>
2019-01-27 18:52:02 -0500

    production env

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.use(bodyParser.json());
 // connect to the database
 const db = require('./config/mongoose');
 
+// serve static assets (build) if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  //load index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // routes
 const items = require('./routes/api/items');
 app.use('/api/items', items);
