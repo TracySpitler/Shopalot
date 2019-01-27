4a4d4fc8aa2f581ef81f50e8088d2f2b5d78bcdf
Tracy Spitler <tracyspitler@gmail.com>
2019-01-27 16:24:20 -0500

    updated propTypes

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -51,8 +51,8 @@ class ShoppingList extends Component{
 }
 
 ShoppingList.propTypes = {
-  getItems: PropTypes.func.isRequierd,
-  item: PropTypes.object.isRequierd
+  getItems: PropTypes.func,
+  item: PropTypes.object
 }
 
 const mapStateToProps = (state) => ({
