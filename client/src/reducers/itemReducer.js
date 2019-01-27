3c9e02a785638b872fb44eb73be5664be85e88e2
Tracy Spitler <tracyspitler@gmail.com>
2019-01-27 13:04:40 -0500

    updated client/src/reducers/itemReducer.js - created a case for ITEMS_LOADING

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,5 +1,5 @@
 import uuid from 'uuid';
-import {  GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import {  GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
   items: [
@@ -14,7 +14,9 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return {
-        ...state
+        ...state,
+        items: action.payload,
+        loading: false
       };
     case DELETE_ITEM:
       return {
@@ -26,6 +28,11 @@ export default function(state = initialState, action) {
       return {
         ...state,
         items: [action.payload, ...state.items]
+      };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true
       };
     default:
       return state;
