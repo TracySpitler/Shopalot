046ee335406b34c27c00af74eb4637d3e3f8ecd0
Tracy Spitler <tracyspitler@gmail.com>
2019-01-27 10:58:46 -0500

    updated ShoppingList.js - added function to delete an item

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
-import { getItems } from '../actions/itemActions';
+import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
+import uuid from 'uuid';
 
 class ShoppingList extends Component{
   // load the list from store.js
   componentDidMount() {
     this.props.getItems();
+  }
+
+  // delete function
+  onDeleteClick = (id) => {
+    this.props.deleteItem(id);
   }
 
     render(){
@@ -40,12 +45,7 @@ class ShoppingList extends Component{
                                     className="remove-btn"
                                     color="danger"
                                     size="sm"
-                                    onClick={() => {
-                                        this.setState(state => ({
-                                            //item by id is filtered out here (deleted)
-                                            items: state.items.filter(item => item.id !== id)
-                                        }));
-                                    }}
+                                    onClick={this.onDeleteClick.bind(this, id)}
                                 >&times;</Button>
                                 {name}</ListGroupItem>
                             </CSSTransition>
@@ -67,4 +67,4 @@ const mapStateToProps = (state) => ({
   item: state.item
 });
 
-export default connect(mapStateToProps, { getItems })(ShoppingList);
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
