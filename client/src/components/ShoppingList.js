1b65d1b41498327d0e510add2ee311cc08b18698
Tracy Spitler <tracyspitler@gmail.com>
2019-01-27 23:20:52 -0500

    troubleshooting deployment issues

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -24,27 +24,26 @@ class ShoppingList extends Component{
         return (
 
             <Container>
+
                 <ItemModal />
 
-                <Container>
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({_id, name}) => (
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                <Button
+                                    className="remove-btn"
+                                    color="danger"
+                                    size="sm"
+                                    onClick={this.onDeleteClick.bind(this, _id)}
+                                >&times;</Button>
+                                {name}</ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
 
-                  <ListGroup>
-                      <TransitionGroup className="shopping-list">
-                          {items.map(({_id, name}) => (
-                              <CSSTransition key={_id} timeout={500} classNames="fade">
-                                  <ListGroupItem>
-                                  <Button
-                                      className="remove-btn"
-                                      color="danger"
-                                      size="sm"
-                                      onClick={this.onDeleteClick.bind(this, _id)}
-                                  >&times;</Button>
-                                  {name}</ListGroupItem>
-                              </CSSTransition>
-                          ))}
-                      </TransitionGroup>
-                  </ListGroup>
-                </Container>
             </Container>
 
         );
