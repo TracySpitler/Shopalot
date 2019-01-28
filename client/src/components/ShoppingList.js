6e10a822382d49b4252598437f8b28fd9c0ec48f
Tracy Spitler <tracyspitler@gmail.com>
2019-01-27 19:23:48 -0500

    troubleshooting deployment issues

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -27,23 +27,7 @@ class ShoppingList extends Component{
 
                 <ItemModal />
 
-                <ListGroup>
-                    <TransitionGroup className="shopping-list">
-                        {items.map(({_id, name}) => (
-                            <CSSTransition key={_id} timeout={500} classNames="fade">
-                                <ListGroupItem>
-                                <Button
-                                    className="remove-btn"
-                                    color="danger"
-                                    size="sm"
-                                    onClick={this.onDeleteClick.bind(this, _id)}
-                                >&times;</Button>
-                                {name}
-                                </ListGroupItem>
-                            </CSSTransition>
-                        ))}
-                    </TransitionGroup>
-                </ListGroup>
+                
             </Container>
 
         );
--- client/src/store.js
+++ client/src/store.js
@@ -6,18 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
-// Developer tools middleware
-const composeSetup =
-  process.env.NODE_ENV !== "production" &&
-  typeof window === "object" &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : compose;
-
-const store = createStore(
+const store = createStore (
   rootReducer,
   initialState,
-  composeSetup(applyMiddleware(...middleware))
+  compose (
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
 );
 
 export default store;
