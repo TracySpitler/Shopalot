d3b1e69e2d508c22b9992297f22d8df472d23f76
Cordell Radke <csradke@fullsail.edu>
2019-02-01 20:53:40 -0800

    Verification for login on server-side.

--- routes/api/signin.js
+++ routes/api/signin.js
@@ -164,4 +164,43 @@ const UserSession = require('../../models/UserSession');
         });
     });
 
+    /**
+     * Verify
+     */
+    router.get('/verify', (req, res, next)=>{
+        // Get the token
+        const { query } = req;
+        const { token } = query;
+        //?token=test
+        // Verify the token is one of a kind and it's not deleted
+
+        UserSession.find({
+            _id: token,
+            isDeleted: false
+        }, (err, sessions) =>{
+            if(err){
+                return res.send({
+                    success: false,
+                    message: 'Error: Server error'
+                });
+            }
+
+            if(sessions.length != 1){
+                return res.send({
+                    success: false,
+                    message: 'Error: Invalid'
+                });
+            } else{
+                return res.send({
+                    success: false,
+                    message: 'Successful Verification!'
+                });
+
+            }
+        });
+        
+    });
+
+    
+
      module.exports = router;
