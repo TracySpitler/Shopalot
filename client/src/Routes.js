16bc383568987dc7d011bdf20945c105af9b57ea
Tracy Spitler <tracyspitler@gmail.com>
2019-02-03 14:23:25 -0500

    fixed NotFound error

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -7,13 +7,15 @@ import Splash from './views/Splash';
 import Signup from './components/Signup';
 import Browse from './components/Browse';
 import ItemInfo from './components/ItemInfo';
+import NotFound from './components/NotFound';
 
 const AppRouter = () => {
   return <Switch>
       <Route path="/" exact component={Splash} />
       <Route path="/shoppinglist" component={Browse} />
       <Route path="/signup" component={Signup} />
-      <Route path="/:id" component={ItemInfo} />
+      <Route path="/item/:id" component={ItemInfo} />
+      <Route path="*" component={NotFound} />
     </Switch>
 };
 
