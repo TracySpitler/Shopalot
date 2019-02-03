c718e8dc2ff2540f08b49ac6e07a6292917a7788
Cordell Radke <csradke@fullsail.edu>
2019-02-03 00:39:34 -0800

    No need for bcrypt in this file

--- models/user.js
+++ models/user.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 // Create Schema
 const UserSchema = new Schema({
 
-    first_name: {
+    firstName: {
         type: String,
         default: '',
         required: true
     },
-    last_name: {
+    lastName: {
         type: String,
         default: ''
     },
@@ -23,12 +24,20 @@ const UserSchema = new Schema({
         default: '',
         required: true
     },
-    date: {
-        type: Date,
-        default: Date.now
+    isDeleted: {
+        type: Boolean,
+        default: false
     }
 
 
 });
 
-module.exports = User = mongoose.model('users', UserSchema);
+UserSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = User = mongoose.model('User', UserSchema);
