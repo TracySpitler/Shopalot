4f6d4a7c58ff6c8eb1d4dc7750ecc8edf55ec99e
Cordell Radke <radke5555@hotmail.com>
2019-02-02 16:53:52 -0800

    Got rid of path /signup
    Everything should be done within /login path

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -14,7 +14,6 @@ const AppRouter = () => {
       <Route path="/" exact component={Splash} />
       <Route path="/shoppinglist" component={ShoppingList} />
       <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
       <Route path="*" component={NotFound} />
     </Switch>
 };
